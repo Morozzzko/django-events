c2670825cc78de6ac210989534a80f5b679c44a6
Igor S. Morozov <igorsmorozov@gmail.com>
2015-05-23 00:03:46 +0300

    chore: edit Event fields' verbose names so they compile with conventions
    Convention is not to capitalize the first letter of verbose names. Django will do it automatically when needed.

--- events/models.py
+++ events/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 from django.utils.translation import ugettext_lazy as _
 
 
 class Event(models.Model):
     name = models.CharField(
-        verbose_name=_('Event name'),
+        verbose_name=_('event name'),
         max_length=50
     )
-    start_date = models.DateTimeField(verbose_name=_('Start date'))
-    end_date = models.DateTimeField(verbose_name=_('End date'))
-    description = models.TextField(verbose_name=_('Description'),
-                                   default='')
+    start_date = models.DateTimeField(verbose_name=_('start date'))
+    end_date = models.DateTimeField(verbose_name=_('end date'))
+    description = models.TextField(verbose_name=_('description'),
+                                   blank=True)
+    organizer = models.ForeignKey(User, verbose_name=_('organizer'))
 
     def __str__(self):
         return self.name
