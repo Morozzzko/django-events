83b33ffd01750b630feac3ba9721dce6d418cf8d
Igor S. Morozov <igorsmorozov@gmail.com>
2015-07-16 18:42:18 +0300

    feat: extended user profile

--- events/models.py
+++ events/models.py
@@ -10,6 +10,39 @@ from django.utils.translation import ugettext_lazy as _
 
 from django.utils.encoding import python_2_unicode_compatible
 
+from phonenumber_field.modelfields import PhoneNumberField
+
+from django_enumfield import enum
+
+
+class PresenceStatus(enum.Enum):
+    ABSENT = 0
+    PRESENT = 1
+    LEFT = 2
+
+    labels = {
+        ABSENT: _('absent'),
+        PRESENT: _('present'),
+        LEFT: _('left')
+    }
+
+
+@python_2_unicode_compatible
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    middleName = models.CharField(verbose_name=_('middle name'),
+                                  max_length=30,
+                                  blank=True)
+    phoneNumber = PhoneNumberField(verbose_name=_('phone number'),
+                                   blank=True)
+
+    status = enum.EnumField(PresenceStatus,
+                            verbose_name=_('presence status'),
+                            default=PresenceStatus.ABSENT)
+
+    def __str__(self):
+        return str(self.user)
+
 
 @python_2_unicode_compatible
 class Event(models.Model):
@@ -21,83 +54,6 @@ class Event(models.Model):
     end_date = models.DateTimeField(verbose_name=_('end date'))
     description = models.TextField(verbose_name=_('description'),
                                    blank=True)
-    organizer_groups = models.ManyToManyField(settings.AUTH_USER_MODEL,
-                                              through='Organizer',
-                                              through_fields=['event', 'user'],
-                                              verbose_name=_('organizers'),
-                                              related_name='event_organizers')
-
-    attendees = models.ManyToManyField(settings.AUTH_USER_MODEL,
-                                       through='Attendee',
-                                       through_fields=['event', 'user'],
-                                       verbose_name=_('attendees'),
-                                       related_name='event_attendees')
 
     def __str__(self):
         return self.name
-
-
-@python_2_unicode_compatible
-class Attendee(models.Model):
-    """
-    Attendee is a user attending an event.
-
-    Attendees have several statuses:
-
-        1. Registration pending (Status.PENDING) -- used whenever organizer needs to personally accept or reject people
-        2. Registered/Registration accepted (status.REGISTERED)
-        3. Attended (status.ATTENDED) -- used to mark person's presence at the event
-        4. Left (status.LEFT) -- used to mark people who left the event for various reasons
-    """
-
-    class Status:
-        PENDING = 'pending'
-        REGISTERED = 'registered'
-        ATTENDED = 'attended'
-        LEFT = 'left'
-
-        choices = (
-            (PENDING, 'pending'),
-            (REGISTERED, 'registered'),
-            (ATTENDED, 'attended'),
-            (LEFT, 'left'),
-        )
-
-    event = models.ForeignKey(Event)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    status = models.CharField(max_length=15,
-                              choices=Status.choices,
-                              verbose_name=_('status'))
-
-    def __str__(self):
-        return _('{user} status on {event}: {status}') % {
-            'user': self.user.username,
-            'event': str(self.event),
-            'status': _(self.status),
-        }
-
-
-@python_2_unicode_compatible
-class Organizer(models.Model):
-    class Roles:
-        HEAD = 'head'
-        REGULAR = 'regular'
-
-        choices = (
-            (HEAD, 'head'),
-            (REGULAR, 'regular')
-        )
-
-    event = models.ForeignKey(Event)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    role = models.CharField(max_length=15,
-                            choices=Roles.choices,
-                            verbose_name=_('role'))
-
-    def __str__(self):
-        return _('{event} organizer ({role}) {user} ') % {
-            'user': self.user.username,
-            'event': str(self.event),
-            'role': _(self.role),
-
-        }
