d3d8d39a0663211395b0fcb52d5ab1ed6338656b
Igor S. Morozov <igorsmorozov@gmail.com>
2015-08-08 01:26:14 +0300

    refactor: rework User serializers

--- events/serializers.py
+++ events/serializers.py
@@ -7,62 +7,43 @@ from rest_framework import serializers
 from .models import Profile, Team
 
 
-# class ProfileSerializer(serializers.HyperlinkedModelSerializer):
-#     id = serializers.ReadOnlyField(source='user.pk')
-#     username = serializers.CharField(source='user.username')
-#     given_name = serializers.CharField(source='user.first_name')
-#     family_name = serializers.CharField(source='user.last_name')
-#     email = serializers.EmailField(source='user.email')
-#     is_staff = serializers.BooleanField(source='user.is_staff')
-#
-#     class Meta:
-#         model = Profile
-#         fields = ('id', 'username', 'given_name', 'additional_name', 'family_name', 'email', 'telephone', 'status',
-#                   'is_staff', 'url', )
-#         extra_kwargs = {
-#             'url': {'view_name': 'user-detail', 'lookup_field': 'pk'},
-#             'users': {'lookup_field': 'username'}
-#         }
-
-
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Team
-        fields = ('')
-
-
-class MinimalUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'url', )
+        model = Profile
+        fields = ('additional_name', 'birth_date', 'telephone', )
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'is_staff', 'url')
+        fields = ('id', 'username', 'email', 'is_staff', 'first_name', 'last_name', 'url', )
 
-    def to_representation(self, obj):
-        pass
+    def to_representation(self, instance):
+        representation_user = serializers.ModelSerializer.to_representation(self, instance)
+        profile = instance.profile
+        representation_profile = ProfileSerializer(context=self.context).to_representation(profile)
+        for key in ProfileSerializer.Meta.fields:
+            representation_user[key] = representation_profile[key]
+        return representation_user
 
     def to_internal_value(self, data):
-        pass
+        return serializers.ModelSerializer.to_internal_value(self, data)
 
 
 class TeamSerializer(serializers.ModelSerializer):
     curator = serializers.HyperlinkedRelatedField(view_name='user-detail',
                                                   queryset=User.objects.all(),
                                                   allow_null=True)
-    members = MinimalUserSerializer(many=True, read_only=True)
+    members = UserSerializer(many=True, read_only=True)
 
     class Meta:
         model = Team
-        fields = ('id', 'name', 'description', 'curator', 'members', 'url')
+        fields = ('id', 'name', 'description', 'curator', 'members', 'url', )
 
     def to_representation(self, instance):
         result = serializers.ModelSerializer.to_representation(self, instance)
         if instance.curator:
-            result['curator'] = MinimalUserSerializer(context=self.context).to_representation(instance.curator)
+            result['curator'] = UserSerializer(context=self.context).to_representation(instance.curator)
         return result
 
 
