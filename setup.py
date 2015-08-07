8ff9dddb822a29366e6ca7f7e6abc0ea19edcecf
Igor S. Morozov <igorsmorozov@gmail.com>
2015-08-07 22:35:16 +0300

    feat: add CamelCaseJSON serializer as dependency

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
     install_requires=[
         'django-phonenumber-field',
         'django-enumfield',
+        'djangorestframework-camel-case',
     ],
     classifiers=[
         'Framework :: Django',
