3d8f693705257b088ad8bfbf2770adb44aa8418d
Igor S. Morozov <moroz@fastmail.com>
2015-08-22 23:54:17 +0300

    feat: add a URL wrapper that adds optiional trailing slashes

--- events/urls.py
+++ events/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.routers import DefaultRouter
 from . import views
+from .utils import add_optional_trailing_slashes
 
 router = DefaultRouter()
 
@@ -35,12 +36,13 @@ team_detail = views.TeamViewSet.as_view({
     'delete': 'destroy'
 })
 
-urlpatterns = format_suffix_patterns([
-    url(r'^users/$', user_list, name='user-list'),
-    url(r'^users/attendees/$', user_list, {'type': 'attendees'}, name='user-list'),
-    url(r'^users/organizers/$', user_list, {'type': 'organizers'}, name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail'),
-    url(r'^users/(?P<pk>[0-9]+)/status/$', user_status, name='user-status'),
-    url(r'^groups/$', team_list, name='team-list'),
-    url(r'^groups/(?P<pk>[0-9]+)/$', team_detail, name='team-detail'),
-], allowed=['json', 'html'])
+urlpatterns = format_suffix_patterns(
+    add_optional_trailing_slashes([
+        url(r'^users/$', user_list, name='user-list'),
+        url(r'^users/attendees/$', user_list, {'type': 'attendees'}, name='user-list'),
+        url(r'^users/organizers/$', user_list, {'type': 'organizers'}, name='user-list'),
+        url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail'),
+        url(r'^users/(?P<pk>[0-9]+)/status/$', user_status, name='user-status'),
+        url(r'^groups/$', team_list, name='team-list'),
+        url(r'^groups/(?P<pk>[0-9]+)/$', team_detail, name='team-detail'),
+    ]), allowed=['json', 'html'])
