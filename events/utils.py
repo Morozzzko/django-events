691bbeead42fea0b1359de2943b212ec9d48d19a
Igor S. Morozov <moroz@fastmail.com>
2015-08-23 17:01:46 +0300

    refactor: improved optional trailing slashes behavior
    In recent version, in order to make the slashes 'optional' we used this regex: [/]?
    It resulted in violation of Django's URL conventions, because API and reverse() returned URLs with NO slash.
    The 'bug' could bring in disastorous misbehavior and had to be resolved

--- events/utils.py
+++ events/utils.py
@@ -18,17 +18,19 @@ def add_optional_trailing_slashes(urlpatterns):
     for pattern in urlpatterns:
         # check whether URL is nested or not
         if isinstance(pattern, RegexURLResolver):
-            regex = pattern.regex.pattern.rstrip('$').rstrip('/') + r'[/]?$'
+            regex = pattern.regex.pattern.rstrip('$').rstrip('/')
             patterns = add_optional_trailing_slashes(pattern.url_patterns)
             namespace = pattern.namespace
             app_name = pattern.app_name
             kwargs = pattern.default_kwargs
-            urls.append(url(regex, include(patterns, namespace, app_name), kwargs))
+            urls.append(url(regex + '$', include(patterns, namespace, app_name), kwargs))
+            urls.append(url(regex + '/$', include(patterns, namespace, app_name), kwargs))
 
         elif isinstance(pattern, RegexURLPattern):
-            regex = pattern.regex.pattern.rstrip('$').rstrip('/') + r'[/]?$'
+            regex = pattern.regex.pattern.rstrip('$').rstrip('/')
             view = pattern._callback or pattern._callback_str
             kwargs = pattern.default_args
             name = pattern.name
-            urls.append(url(regex, view, kwargs, name))
+            urls.append(url(regex + '$', view, kwargs, name))
+            urls.append(url(regex + '/$', view, kwargs, name))
     return urls
