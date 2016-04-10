1cb7b668139e0fa40fb105e44ef0531509a720b5
Igor S. Morozov <igorsmorozov@gmail.com>
2016-04-10 08:41:46 +0300

    Add site title and user name to side panel

--- events/frontend/views.py
+++ events/frontend/views.py
@@ -6,4 +6,6 @@ from django.shortcuts import render
 
 @login_required
 def frontend_view(request):
-    return render(request, 'events/frontend/index.html')
+    return render(request, 'events/frontend/index.html', {
+        'site_title': 'Event management',
+    })
